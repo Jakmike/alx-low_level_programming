1195fe885b387b2baac863b10226e14a4e650082
Jakmike <asomughakenenna@gmail.com>
2022-09-08 14:48:09 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,41 @@
 /*
- * File: 4-puts.c
- * Auth: Brennan D Baraban <375@holbertonschool.com>
- * */
  *
- * #include <stdio.h>
+ *  * File: 4-puts.c
  *
- * /**
+ *  * Auth: Brennan D Baraban <375@holbertonschool.com>
+ *
+ *  */
+
+
+
+#include <stdio.h>
+
+
+
+/**
+ *
  *  * main - Prints "Programming is like building a multilingual
+ *
  *  *                puzzle, followed by a new line.
  *
+ *  *
+ *
  *  * Return: Always 0.
+ *
  *  */
- int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+
+
+			return (0);
+
 }
+
+Footer
+
+© 2022 GitHub, Inc.
