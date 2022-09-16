2dd82c358e36bd75ae20de07f1431be9030387be
Jakmike <asomughakenenna@gmail.com>
2022-09-16 11:48:09 -0700

    Triangle

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,40 +7,40 @@
  */
 void print_number(int n)
 {
-        int i;
-        int j = 1;
-        unsigned int x = n;
-        unsigned int y = n;
-        int a = 0;
+	int i;
+	int j = 1;
+	unsigned int x = n;
+	unsigned int y = n;
+	int a = 0;
 
-        if (n == 0)
-        {
-                _putchar('0');
-        }
-        if (n < 0)
-        {
-                _putchar('-');
-                n = n + 1;
-                n = -n;
-                y = n;
-                x = n;
-                x += 1;
-                y += 1;
-        }
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n + 1;
+		n = -n;
+		y = n;
+		x = n;
+		x += 1;
+		y += 1;
+	}
 
-        while (x != 0)
-        {
-                x = x / 10;
-                a++;
-        }
-        for (i = 1; i < a; i++)
-        {
-                j *= 10;
-        }
-        for (i = 0; i < a; i++)
-        {
-                _putchar(y / j + '0');
-                y = y % j;
-                j = j / 10;
-        }
+	while (x != 0)
+	{
+		x = x / 10;
+		a++;
+	}
+	for (i = 1; i < a; i++)
+	{
+		j *= 10;
+	}
+	for (i = 0; i < a; i++)
+	{
+		_putchar(y / j + '0');
+		y = y % j;
+		j = j / 10;
+	}
 }
