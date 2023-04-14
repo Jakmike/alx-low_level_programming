553e41a6628d325b1e655315e7522039fc19ebca
Jakmike <asomughakenenna@gmail.com>
2023-04-14 02:14:54 -0700

    task 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,8 +1,7 @@
 #include "search_algos.h"
 
 /**
- * linear_search - searching for a value in an array of
- * integers
+ * linear_search - searching for a value in an array of integers
  *
  * @array: input array
  * @size: size of the array
